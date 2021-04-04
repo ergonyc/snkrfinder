8d8b10af8e355fb1a0a90de7f50bb24f8678d94b
ergonyc <ergonyc@pizzapie.lan>
2021-04-03 23:35:30 -0700

    remade docs / module

--- snkrfinder/__init__.py
+++ snkrfinder/__init__.py
@@ -1,12 +1,1 @@
 __version__ = "0.0.1"
-
-# from .core import *
-
-# from .data import *
-
-# from .model import *
-# # from .model.core import *
-# # from .model.transfer import *
-# # from .model.cvae import *
-
-# from .widgets import *
